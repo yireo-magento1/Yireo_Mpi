f20d7ba3c283d6635c70d97cb0d6cc9686f9cfa8
Jisse Reitsma <info@yireo.com>
2015-11-03 21:36:20 +0100

    Pending changes

---
+++ source/app/code/community/Yireo/Mpi/Block/System/Config/Form/Field/Version.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Yireo Mpi for Magento
+ *
+ * @package     Yireo_Mpi
+ * @author      Yireo (http://www.yireo.com/)
+ * @copyright   Copyright 2015 Yireo (http://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+/**
+ * System check class
+ */
+class Yireo_Mpi_Block_System_Config_Form_Field_Version extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    /**
+     * @param Varien_Data_Form_Element_Abstract $element
+     *
+     * @return string
+     */
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        $config = Mage::app()->getConfig()->getModuleConfig('Yireo_Mpi');
+        return (string)$config->version;
+    }
+}
